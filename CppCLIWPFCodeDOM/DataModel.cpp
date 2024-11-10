89f96394b7baa672a4da22457e1a0b7bbd707032
Krzysztof Napiórkowski <KrzysiuN@proton.me>
2024-11-10 19:38:14 +0100

    initial

---
+++ CppCLIWPFCodeDOM/DataModel.cpp
@@ -0,0 +1,24 @@
+#include "DataModel.h"
+
+DataModel::DataModel(const String^ codeText)
+{
+	_codeText = _codeText;
+}
+
+DataModel::NavigateToAddCodeCommand::NavigateToAddCodeCommand(DataModel^ viewModel)
+{
+    _viewModel = viewModel;
+}
+
+bool DataModel::NavigateToAddCodeCommand::CanExecute(System::Object^ parameter) 
+{
+    return true;
+}
+
+void DataModel::NavigateToAddCodeCommand::Execute(System::Object^ parameter)
+{
+    _viewModel->ErrorsList->Clear();
+    _viewModel->ErrorsList->Add("Not implemented yet");
+}
+
+
